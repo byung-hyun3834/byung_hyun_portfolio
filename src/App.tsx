7d7cd570f5c57c9a2b80b36e23e4f10e6f547d0f
byung_hyun <sbh3834@naver.com>
2024-07-15 22:47:15 +0900

    포폴 사이트 cra > vite 마이그레이션 작업중

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import { CustomRoute } from "./routes";
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="*" element={<CustomRoute></CustomRoute>} />
+      </Routes>
+    </HashRouter>
+  );
+}
